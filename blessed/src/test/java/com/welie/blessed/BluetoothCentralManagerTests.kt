e3714f9704b29263efdc98159212a83f988d9493
Martijn van Welie <martijn.van.welie@gmail.com>
2023-03-26 09:44:19 +0200

    Get first tests working

--- blessed/src/test/java/com/welie/blessed/BluetoothCentralManagerTests.kt
+++ blessed/src/test/java/com/welie/blessed/BluetoothCentralManagerTests.kt
@@ -1,10 +1,11 @@
 package com.welie.blessed
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothClass.Device
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.*
+import android.bluetooth.le.ScanSettings.CALLBACK_TYPE_ALL_MATCHES
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.IntentFilter
@@ -22,6 +23,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowLooper
+
 
 @RunWith(RobolectricTestRunner::class)
 class BluetoothCentralManagerTests {
@@ -65,12 +67,12 @@ class BluetoothCentralManagerTests {
             null
         }
 
-        callback = mockk<BluetoothCentralManagerCallback>()
+        callback = mockk<BluetoothCentralManagerCallback>(relaxed = true)
         central = BluetoothCentralManager(context, callback, Handler(Looper.getMainLooper()) )
     }
 
     @Test
-    fun `When a scanForPeripherals is called, then startScan is called without filters`() {
+    fun `When a scanForPeripherals is called, then a scan without filters is started `() {
         // Given
         every { bluetoothAdapter.isEnabled } returns true
 
@@ -81,5 +83,49 @@ class BluetoothCentralManagerTests {
         val filters = slot<List<ScanFilter>>()
         verify { scanner.startScan(capture(filters), any(), any< ScanCallback>()) }
         assertEquals(0, filters.captured.size)
+    }
+
+    @Test
+    fun `When an unfiltered scan is started, then ScanResults and Peripherals will be received`() {
+        // Given
+        val scanCallback = slot<ScanCallback>()
+        val device = getDevice()
+        val scanResult = getScanResult(device)
+        every { bluetoothAdapter.isEnabled } returns true
+        every { bluetoothAdapter.getRemoteDevice(DEVICE_ADDRESS) } returns device
+
+        // When
+        central.scanForPeripherals()
+        verify { scanner.startScan(any(), any(), capture(scanCallback)) }
+        scanCallback.captured.onScanResult(CALLBACK_TYPE_ALL_MATCHES, scanResult)
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+
+        // Then
+        val peripheral = slot<BluetoothPeripheral>()
+        val receivedScanResult = slot<ScanResult>()
+        verify { callback.onDiscoveredPeripheral(capture(peripheral), capture(receivedScanResult)) }
+        assertEquals(scanResult, receivedScanResult.captured)
+        assertEquals(device.address, peripheral.captured.address)
+        assertEquals(device.name, peripheral.captured.name)
+    }
+
+    private fun getScanResult(device: BluetoothDevice): ScanResult {
+        val scanRecord = mockk<ScanRecord>()
+        val scanResult = mockk<ScanResult>()
+        every { scanResult.device } returns device
+        every { scanResult.scanRecord } returns scanRecord
+        return scanResult
+    }
+
+    private fun getDevice(): BluetoothDevice {
+        val device = mockk<BluetoothDevice>()
+        every { device.address } returns DEVICE_ADDRESS
+        every { device.name } returns DEVICE_NAME
+        return device
+    }
+
+    companion object {
+        private const val DEVICE_ADDRESS = "12:23:34:45:56:67"
+        private const val DEVICE_NAME = "MyDevice"
     }
 }
